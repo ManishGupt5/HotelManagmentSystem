d73d800f90db1e16253633518dfc44faaad92029
Manishgupt5 <manishgupt9211@gmail.com>
2023-03-31 21:05:49 +0530

    day-4

---
+++ data.js
@@ -0,0 +1,137 @@
+let menu = [
+    {
+        category: "breakfast",
+        img: "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Sides/Sides%20PDP/_0000s_0010_%5BFeed%5D_0003s_0002_Sides_Fruit-Cup.png",
+        name: "Fruit Cup",
+        details: "60 Cal per Serving",
+    },
+    {
+        category: "breakfast",
+        img: "https://www.cfacdn.com/img/order/menu/Online/Sides%26treats/Jul19_OrganicAussieGreekYogurtParfaitCupGranola_pdp.png",
+        name: "Greek Yogurt Parfai",
+        details: "270 Cal per serving",
+    },
+    {
+        category: "breakfast",
+        img: "https://www.cfacdn.com/img/order/menu/Online/Breakfast/EMuffin_PDP.png",
+        name: "English Muffin",
+        details: "140 Cal per muffin",
+    },
+    {
+        category: "sandwich",
+        img: "https://www.cfacdn.com/img/order/menu/Online/Breakfast/CECMuffin_1080px.png",
+        name: "Chicken, Egg & Cheese Muffin",
+        details: "420 Cal per Sandwich",
+    },
+    {
+        category: "sandwich",
+
+        img: "https://www.cfacdn.com/img/order/menu/Online/Entrees/Jul19_CFASandwich_pdp.png",
+        name: "Chick-fil-A® Chicken Sandwich",
+        details: "440 Cal per Sandwich",
+    },
+    {
+        category: "sandwich",
+        img: "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Entree/Entree%20PDP/_0000s_0004_NEWStack6200001CFAPDPDeluxeSandwich1085png.png",
+        name: "Chick-fil-A® Deluxe Sandwich",
+        details: "500 Cal per Sandwich",
+    },
+    {
+        category: "sandwich",
+        img: "https://www.cfacdn.com/img/order/menu/Online/Entrees/spicyChilledGrilled_PDP.png",
+        name: "Spicy Chilled Grilled Chicken Sub Sandwich",
+        details: "430 Cal per sandwich",
+    },
+    {
+        category: "sandwich",
+        img: "https://www.cfacdn.com/img/order/menu/Online/Entrees/CFASpicySandwich_1080.png",
+        name: "Spicy Chicken Sandwich",
+        details: "460 Cal per Sandwich",
+    },
+    {
+        category: "kid",
+        img: "https://www.cfacdn.com/img/order/menu/Online/Entrees/Dec2019_CFANuggets_5ct_pdp.png",
+        name: "5 Ct Nuggets Kid's Meal",
+        details: "160 Cal per Entree",
+    },
+    {
+        category: "kid",
+        img: "https://www.cfacdn.com/img/order/menu/Online/Entrees/strips_2ct_PDP.png",
+        name: "2 Ct Chick-n-Strips® Kid's Meal",
+        details: "200 Cal per entrée",
+    },
+    {
+        category: "kid",
+        img: "https://www.cfacdn.com/img/order/menu/Online/Entrees/Dec2019_GrilledNuggets_5ct_pdp.png",
+        name: "5 Ct Grilled Nuggets Kid's Meal",
+        details: "80 Cal per Entree",
+    },
+    {
+        category: "drink",
+        img: "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Drinks/Drinks%20PDP/_0000s_0022_Feed_Menu_0000_Drinks_Coca-cola.png",
+        name: "Coca-Cola®",
+        details: "140 Cal per Container",
+    },
+    {
+        category: "drink",
+        img: "https://www.cfacdn.com/img/order/menu/Mobile/Beverages/Parent/DrPepper_PaperCup_straight_wet_1200x1200.png",
+        name: "Dr Pepper®",
+        details: "180 Cal per Container",
+    },
+    {
+        category: "drink",
+        img: "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Drinks/Drinks%20PDP/_0000s_0026_%5BFeed%5D_0006s_0014_Drinks_Dasani-Water.png",
+        name: "DASANI® Bottled Water",
+        details: "0 Cal per Bottle",
+    },
+
+    {
+        category: "treat",
+        img: "https://www.cfacdn.com/img/order/menu/Online/Sides%26treats/PDP_CCCookie.png",
+        name: "Chocolate Chunk Cookie",
+        details: "370 Cal per cookie",
+    },
+    {
+        category: "treat",
+        img: "https://www.cfacdn.com/img/order/menu/Mobile/Desserts/Menu%20Item/16oz_C%26C_Milkshake-1080.png",
+        name: "Chocolate Milkshake",
+        details: "590 Cal per Milkshake",
+    },
+    {
+        category: "treat",
+        img: "https://www.cfacdn.com/img/order/menu/Online/Sides%26treats/IceDream_RedCup_1080x1080.png",
+        name: "Chick-fil-A® Icedream® Cup",
+        details: "140 Cal per cup",
+    },
+    {
+        category: "treat",
+        img: "https://www.cfacdn.com/img/order/menu/Mobile/Desserts/Menu%20Item/16oz_StrawberryMilkshake-1080.png",
+        name: "Strawberry Milkshake",
+        details: "570 Cal per milkshake",
+    },
+    {
+        category: "treat",
+        img: "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Treats/Treats%20Desktop/_0001s_0004_%5BFeed%5D_0005s_0000_Treats_Frosted-Coffee.png",
+        name: "Frosted Coffee",
+        details: "250 Cal per Serving",
+    },
+
+    {
+        category: "meal",
+        img: "https://www.cfacdn.com/img/order/menu/Online/PackagedMeals/Jul19_CFASandwich_PackagedMeal_Cookie_Chips_pdp.png",
+        name: "Chick-fil-A® Chicken Sandwich Packaged Meal",
+        details: "1030 Cal per meal",
+    },
+    {
+        category: "meal",
+        img: "https://www.cfacdn.com/img/order/menu/Online/PackagedMeals/CoolWrapPackagedMeal_1080x1080.png",
+        name: "Chick-fil-A® Cool Wrap Packaged Meal",
+        details: "940 Cal per meal",
+    },
+    {
+        category: "meal",
+        img: "https://www.cfacdn.com/img/order/menu/Online/PackagedMeals/Jul19_SpicySandwich_PackagedMeal_Cookie_Chips_pdp.png",
+        name: "Spicy Chicken Sandwich Packaged Meal",
+        details: "1050 Cal per meal",
+    },
+];
